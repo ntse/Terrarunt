bdc74a324c63363b4d4fe4868405db28b820539f
Sam Ainsworth <sam.ainsworth@ukhsa.gov.uk>
2025-04-10 12:30:27 +0100

    fix tflocal support

--- src/terrarunt/providers.py
+++ src/terrarunt/providers.py
@@ -5,8 +5,14 @@ from terrarunt.custom_logger import get_logger
 
 logger = get_logger()
 
+def is_localstack():
+    return os.getenv("TF_WRAPPER_BIN", "").lower() == "tflocal"
+
 class aws:
     def __init__(self) -> None:
-        client = boto3.client("sts")
-        self.account_id = client.get_caller_identity()["Account"]
+        if is_localstack():
+            self.account_id="000000000000"
+        else:
+            client = boto3.client("sts")
+            self.account_id = client.get_caller_identity()["Account"]
         self.region = os.environ.get("AWS_REGION")
